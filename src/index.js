325ad64b3c191999f007cb7b920a2add922bea18
Chad C <github@thechad.com>
2023-06-13 21:16:18 -0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+const functions = require('@google-cloud/functions-framework');
+
+// If changing function name, also update in .github/workflows/deploy.yml
+functions.http('helloHttp', (req, res) => {
+  res.send(`Hello ${req.query.name || req.body.name || 'World'}!`);
+});
