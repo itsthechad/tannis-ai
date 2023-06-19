7d0eed687f10c87b961b19a5ce198a6ab1a5705f
Chad C <github@thechad.com>
2023-06-18 20:22:32 -0400

    experimenting with deployment

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const functions = require('@google-cloud/functions-framework');
 
 // If changing function name, also update in .github/workflows/deploy.yml
 functions.http('helloHttp', (req, res) => {
-  res.send(`Hello ${req.query.name || req.body.name || 'World'}!`);
+  res.send(`Hello ${req.query.name || req.body.name || 'Cruel World'}!`);
 });
