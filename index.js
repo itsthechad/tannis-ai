724814a2e1d91fb2ad040aef034ea7fd408fb605
Chad C <github@thechad.com>
2023-06-19 20:46:46 -0400

    Experiment with Twilio

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const twilio = require('twilio');
-const functions = require('@google-cloud/functions-framework');
+import twilio from 'twilio';
+import { http } from '@google-cloud/functions-framework';
 
-const client = new Twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
+const client = new twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
 
 // If changing function name, also update in .github/workflows/deploy.yml
-functions.http('helloHttp', (req, res) => {
+http('helloHttp', (req, res) => {
   // res.send(`Hello ${req.query.name || req.body.name || 'Cruel World'}!`);
   client.messages.create( {
     to: process.env.MY_PHONE_NUMBER,
